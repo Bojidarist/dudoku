5371268ca7557eb98b97e01dd90364b5277ae324
Poisy <atanas123atanasov@gmail.com>
2020-10-06 00:05:45 +0300

    reformated new_game/dudoku.py

--- dudoku.py
+++ dudoku.py
@@ -21,7 +21,7 @@ class Dudoku():
             x = random.randint(0, 8)
             y = random.randint(0, 8)
             if (self.cells[x][y] == 0):
-                new_value = random.randint(1, 10)
+                new_value = random.randint(1, 9)
                 if (not self.do_exist_in_row(x, new_value) and not self.do_exist_in_column(y, new_value)):
                     self.cells[x][y] = new_value
                     cells_to_create = cells_to_create - 1
@@ -41,7 +41,9 @@ class Dudoku():
     def draw_cells(self):
         for i in range(1, 10):
             for j in range(1, 10):
-                    arcade.draw_rectangle_outline(i * self.cell_width - self.cell_width / 2 , j * self.cell_height - self.cell_height / 2, self.cell_width, self.cell_height, arcade.color.BRITISH_RACING_GREEN, 1)
+                    arcade.draw_rectangle_outline(i * self.cell_width - self.cell_width / 2 , 
+                    	j * self.cell_height - self.cell_height / 2, self.cell_width, 
+                    	self.cell_height, arcade.color.BRITISH_RACING_GREEN, 1)
 
     def draw_bigcells(self):
         w = 3 * self.cell_width
@@ -51,6 +53,10 @@ class Dudoku():
                 arcade.draw_rectangle_outline(i * w - w / 2, j * h - h / 2, w, h, arcade.color.BRITISH_RACING_GREEN, 3)
 
     def draw_playground(self):
-        arcade.draw_rectangle_outline(self.cell_width * 9 - (self.cell_width * 9) / 2, self.cell_height * 9 - (self.cell_height * 9) / 2, self.cell_width * 9, self.cell_height * 9, arcade.color.BRITISH_RACING_GREEN, 10)
+        arcade.draw_rectangle_outline(self.cell_width * 9 - (self.cell_width * 9) / 2, 
+        	self.cell_height * 9 - (self.cell_height * 9) / 2, 
+        	self.cell_width * 9, self.cell_height * 9, 
+        	arcade.color.BRITISH_RACING_GREEN, 10)
+
         self.draw_cells()
         self.draw_bigcells()
